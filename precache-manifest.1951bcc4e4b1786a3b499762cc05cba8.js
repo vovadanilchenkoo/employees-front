b5302952157c5643acf1b751206ca3498d3ea8dc
vova <vovadanilchenkoo@gmail.com>
2019-12-08 14:00:35 +0200

    build for production

---
+++ precache-manifest.1951bcc4e4b1786a3b499762cc05cba8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87b19ea899de37e16c3b27075985d00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa8c44fd9ebb23d32cf",
+    "url": "/static/css/main.943e84fb.chunk.css"
+  },
+  {
+    "revision": "ebda3cce3701079743cd",
+    "url": "/static/js/2.8fb0e81b.chunk.js"
+  },
+  {
+    "revision": "faa8c44fd9ebb23d32cf",
+    "url": "/static/js/main.f8448022.chunk.js"
+  },
+  {
+    "revision": "4e0522dd9550792a66e9",
+    "url": "/static/js/runtime-main.2fd7f418.js"
+  }
+]);
--- src/views/Main/Main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Main = () => (
-    <h1 className="center-align">Welcome to react CRUD app using Node, PostgreSQL, Redis on backend</h1>
-)
-
-export default Main
