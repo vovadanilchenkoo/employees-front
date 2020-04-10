68e3608fc3fdafba5dc3e6c4a4b8393287a5df99
vovadanilchenkoo <vovadanilchenkoo@gmail.com>
2020-04-10 18:16:43 +0300

    v1.22.4

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // apiUrl: 'http://localhost:400000'
-  apiUrl: 'https://api-postgres-crud.herokuapp.com'
+  apiUrl: 'http://localhost:4000'
+  // apiUrl: 'https://api-postgres-crud.herokuapp.com'
 }
 
 export default config
